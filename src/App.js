5e61c0f49c1b310a655f96b7674aaf7cac99c504
diego-bittencourt <mendesdiego90@gmail.com>
2023-01-30 09:20:13 +0900

    basic app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <h1>
+        I'm learning React and I'll a get a job as software engineer THIS YEAR!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
