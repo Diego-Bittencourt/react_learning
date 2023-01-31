911ba9b35d69c37588264ceaf8a2a81b345bc6ce
diego-bittencourt <mendesdiego90@gmail.com>
2023-01-31 10:57:43 +0900

    create a card component as a wrap css component and passing props.children into it

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,6 +3,7 @@
 //repeat the file name
 
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 import "./ExpenseItem.css";
 //you shoudl import the css files in the component
 
@@ -12,14 +13,18 @@ function ExpenseItem(props) {
   //the props have information about the date. This props is redirected to another child component,
 
   //you can only have one root element per each return statement
+
+  //to use the card component as a wrapper to pass content inside it, you have to set
+  //the props.children built-in props to be displayed inside the wrapper component
+  //all components receive that props, even if you don't set it.
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
