63e9a91502753e5b8b7bc3257915ce5b178be757
Diego Bittencourt Mendes <mendesdiego90@gmail.com>
2023-02-21 10:46:51 +0900

    add a card custom component and injecting the classes in the custom component

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,10 +1,12 @@
 import React from "react";
+import styles from "./Card.module.css";
 
 const Card = (props) => {
 
-
+    //if your custom component receives classes, you have to assign it from the props 
+    //like below.
     return (
-        <div></div>
+        <div className={`${styles.card} ${props.className}`}>{props.children}</div>
     )
 };
 
--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,25 +1,29 @@
 import React from "react";
 import styles from "./AddUser.module.css";
+import Card from "../UI/Card";
 
 const AddUser = () => {
-    const addUserHandler = (event) => {
-        
-        //prevent default submission behaviour
-        event.preventDefault();
-    };
+  const addUserHandler = (event) => {
+    //prevent default submission behaviour
+    event.preventDefault();
+  };
 
+  //for custom components, just add className doesn't work. 
+  //you have to go to the custom component and inject the classNames from the props
+  //like this className={props.className} and don't forget to use `` or concatenate to
+  //add other classes that might be used there too.
 
-    return (
-        <Card>
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">UserName</label>
-            <input type="text" id="username" />
-            <label htmlFor="userage">Age (years)</label>
-            <input type="number" id="userage" />
-            <button type="submit">Add User</button>
-        </form>
-        </Card>
-    )
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">UserName</label>
+        <input type="text" id="username" />
+        <label htmlFor="userage">Age (years)</label>
+        <input type="number" id="userage" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
 };
 
 export default AddUser;
