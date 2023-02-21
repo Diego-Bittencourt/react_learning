c2ec6cf576fd534bb3b1e146d3c3fbeb424911a4
Diego Bittencourt Mendes <mendesdiego90@gmail.com>
2023-02-21 16:40:48 +0900

    create the usersList component that renders the list of the array o components.
    each element has the user name, age and receives the index which is generated by Math.random()
    Plus, I added a delete Button that receives the index and removes the element with the same id.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import AddUser from './components/Users/AddUsers';
-
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUsers";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
-
   const addNewUser = (username, userage) => {
-    console.log('in the App.js', username, userage);
+    const user = { name: username, age: userage, index: Math.random().toString() };
+    setUsersList((lastUsersList) => {
+      return [user, ...lastUsersList];
+    });
   };
 
+  const deleteUser = (userIndex) => {
+    setUsersList((lastUsersList) => {
+      return lastUsersList.filter(e => 
+        e.index !== userIndex
+      )
+    })
+  }
+
+  const [usersList, setUsersList] = useState([]);
 
   return (
     <div>
       <AddUser addNewUser={addNewUser} />
+      <UsersList users={usersList} deleteUser={deleteUser} />
     </div>
   );
 }
