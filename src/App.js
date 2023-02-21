c6cc3cbcd1652809f6fd7a8d0d02bca74b51e20a
Diego Bittencourt Mendes <mendesdiego90@gmail.com>
2023-02-21 10:21:37 +0900

    initial commit simples app to practice the basics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import React from 'react';
+
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <h1>
-        I'm learning React and I'll a get a job as software engineer THIS YEAR!
-      </h1>
+
     </div>
   );
 }
