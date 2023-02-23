2f05c5233feacfb0d152ede7122466aa29a39ad6
Diego Bittencourt Mendes <mendesdiego90@gmail.com>
2023-02-23 13:25:50 +0900

    add closing errormodal in the backdrop

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -6,7 +6,7 @@ import styles from "./ErrorModal.module.css";
 const ErrorModal = (props) => {
   return (
     <div>
-      <div className={styles.backdrop}></div>
+      <div className={styles.backdrop} onClick={props.onClick}></div>
       <Card className={styles.modal}>
         <header className={styles.header}>
           <h2>{props.title}</h2>
